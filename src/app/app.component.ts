92cfa0b93e792dea52a5c56dcb216e4254812567
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-07-19 10:14:13 +0700

    add check location home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+// Angular
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   isHome = true;
 
-  constructor(private _route: Router) {
+  constructor(private _location: Location) { }
 
+  ngOnInit() {
+    if (this._location.isCurrentPathEqualTo('')) {
+      this.isHome = false;
+    } else {
+      this.isHome = true;
+    }
+  }
+
+  back() {
+    this._location.back();
   }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this._route.snapshot.url);
   }
 
 }
