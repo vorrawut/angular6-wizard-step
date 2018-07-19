3936280248ff080cd122dea40ea166b9ea10ad39
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-07-19 16:45:03 +0700

    add style and fix nav bar icon

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,7 +10,6 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor(private _route: Router) { }
-
   ngOnInit() { }
 
   goToPrevious(form: any) {
