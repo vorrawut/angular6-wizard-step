db48d61beba5fad7497e921b3204d84bfc951cae
Vorrawut Judasri <vorrawutjud@gmail.com>
2018-07-19 23:21:09 +0700

    edit back btn in user-info-detail

--- src/app/components/user-info-detail/user-info-detail.component.ts
+++ src/app/components/user-info-detail/user-info-detail.component.ts
@@ -1,3 +1,5 @@
+// Angular
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserInfoDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route: Router) { }
 
   ngOnInit() {
+  }
+
+  goToPrevious() {
+    this._route.navigate(['user-info']);
   }
 
 }
